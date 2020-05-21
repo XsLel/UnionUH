7af22ee4bf5ea92e10eeb2cda4eb633115d529ed
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-05-21 19:45:11 -0400

    chore: Add server context path
    Add server.servlet.context-path=/api in application.properties and update endpoint in UserRestController

--- src/main/java/com/umss/dev/training/jtemplate/controller/UserRestController.java
+++ src/main/java/com/umss/dev/training/jtemplate/controller/UserRestController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserRestController {
 
     private UserService service;
