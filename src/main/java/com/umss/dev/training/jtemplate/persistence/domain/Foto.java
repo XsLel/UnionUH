cf06b698bed3f6aa80148d2db428c161685442cc
Miler13 <milerrojassiles@gmail.com>
2020-05-21 09:42:23 -0400

    h2
    agregar dato  en la base de datos

--- src/main/java/com/umss/dev/training/jtemplate/persistence/domain/Foto.java
+++ src/main/java/com/umss/dev/training/jtemplate/persistence/domain/Foto.java
@@ -14,17 +14,34 @@ public class Foto {
 		
 		private int idRes;
 	  	private String	urlfoto;
+	  	private boolean carusel;
 	  
 	 
 	  	public Foto() {}
 
+	  	
+	  	
 
-		public Foto(int idfoto, int idRes, String direccion) {
+		@Override
+		public String toString() {
+			return "Foto [idfoto=" + idfoto + ", idRes=" + idRes + ", urlfoto=" + urlfoto + ", carusel=" + carusel
+					+ "]";
+		}
+
+		
+		
+
+
+
+		public Foto(int idfoto, int idRes, String urlfoto, boolean carusel) {
 			super();
 			this.idfoto = idfoto;
 			this.idRes = idRes;
-			this.urlfoto = direccion;
+			this.urlfoto = urlfoto;
+			this.carusel = carusel;
 		}
+
+
 
 
 		public int getIdfoto() {
@@ -32,9 +49,13 @@ public class Foto {
 		}
 
 
+
+
 		public void setIdfoto(int idfoto) {
 			this.idfoto = idfoto;
 		}
+
+
 
 
 		public int getIdRes() {
@@ -42,22 +63,79 @@ public class Foto {
 		}
 
 
+
+
 		public void setIdRes(int idRes) {
 			this.idRes = idRes;
 		}
 
 
-		public String getDireccion() {
+
+
+		public String getUrlfoto() {
 			return urlfoto;
 		}
 
 
-		public void setDireccion(String direccion) {
-			this.urlfoto = direccion;
+
+
+		public void setUrlfoto(String urlfoto) {
+			this.urlfoto = urlfoto;
 		}
-		
-	  	
-		
+
+
+
+
+		public boolean isCarusel() {
+			return carusel;
+		}
+
+
+
+
+		public void setCarusel(boolean carusel) {
+			this.carusel = carusel;
+		}
+
+
+
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (carusel ? 1231 : 1237);
+			result = prime * result + idRes;
+			result = prime * result + idfoto;
+			result = prime * result + ((urlfoto == null) ? 0 : urlfoto.hashCode());
+			return result;
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Foto other = (Foto) obj;
+			if (carusel != other.carusel)
+				return false;
+			if (idRes != other.idRes)
+				return false;
+			if (idfoto != other.idfoto)
+				return false;
+			if (urlfoto == null) {
+				if (other.urlfoto != null)
+					return false;
+			} else if (!urlfoto.equals(other.urlfoto))
+				return false;
+			return true;
+		}
+
+
 	  	
 	  	
 }
