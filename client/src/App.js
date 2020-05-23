75aad602ce45f7ce7e2711d9564ad9efb3587963
drexx <villarroel24kyle@gmail.com>
2020-05-23 10:39:31 -0800

    añadiendo componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import { history } from "./services";
-import { Admin } from "./Admin/Admin";
-import { PageNotFound } from "./common";
-
+import Footer from "./footer";
+import Listado from "./listado";
+import Header from "./header";
+import Mosaico from "./mosaico";
 function App() {
   return (
     <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={Admin} />
-        <Route exact path="/404" component={PageNotFound} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Listado} />
+          <Route path="/mosaico" exact component={Mosaico} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   );
 }
