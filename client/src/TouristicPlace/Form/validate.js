20aa33ce778f5dff8955bb0a02ddf835c27e6c01
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-05-29 00:07:36 -0400

    refactor: Use destructuring in validate function

--- client/src/TouristicPlace/Form/validate.js
+++ client/src/TouristicPlace/Form/validate.js
@@ -1,22 +1,22 @@
-export default function validate(values) {
+export default function validate({ name, description, address, schedules }) {
   const errors = {};
   const isWhitespace = /^\s+$/m;
 
-  if (!values.name || isWhitespace.test(values.name)) {
+  if (!name || isWhitespace.test(name)) {
     errors.name = "El nombre es requerido, introduce un valor válido.";
-  } else if (!(5 <= values.name.length && values.name.length <= 255)) {
+  } else if (!(5 <= name.length && name.length <= 255)) {
     errors.name = "Solo se aceptan entre 5 y 255 caracteres.";
   }
 
-  if (!values.description || isWhitespace.test(values.description)) {
+  if (!description || isWhitespace.test(description)) {
     errors.description = "La descripción es requerida, introduce un valor válido.";
   }
 
-  if (!values.address || isWhitespace.test(values.address)) {
+  if (!address || isWhitespace.test(address)) {
     errors.address = "La dirección es requerida, introduce un valor válido.";
   }
 
-  if (!values.schedules || isWhitespace.test(values.schedules)) {
+  if (!schedules || isWhitespace.test(schedules)) {
     errors.schedules = "Los horarios son requeridos, introduce valores válidos.";
   }
 
