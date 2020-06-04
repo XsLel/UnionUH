842a748363f25e56db1179dcd88701b211ba5ad8
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-06-04 19:39:15 -0400

    Add toasts notifications (#28)
    umss #36 Add toasts notifications

--- client/src/TouristicPlace/Form/useForm.js
+++ client/src/TouristicPlace/Form/useForm.js
@@ -28,6 +28,7 @@ const useForm = (callback, validate) => {
   React.useEffect(() => {
     if (Object.keys(errors).length === 0 && isSubmitting) {
       callback();
+      setIsSubmitting(false);
     }
   }, [errors, isSubmitting, callback]);
 
