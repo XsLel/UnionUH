bf12ce9ff38c7aaddd1b900ad26becfd5abdb640
Roger <molleja_rs@hotmail.com>
2020-06-06 13:09:14 -0400

    se refactorizo el id=botonesprincipales

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import { history } from "./services";
 import { Admin } from "./Admin/Admin";
 import { PageNotFound } from "./common";
 
-import AddComidas from "./FoodAdd/Form"
-import Administrador from "./FoodAdd/pages/Administrador"
+import AddComidas from "./FoodAdd/Form";
+import Administrador from "./FoodAdd/pages/Administrador";
 function App() {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={Admin} />
         <Route exact path="/restaurants/Comida" component={AddComidas} />
-        <Route exact path="/prueba/administrador" component={Administrador}/>
+        //
+        <Route exact path="/restaurants/food" component={AddComidas} />
+        <Route exact path="/prueba/administrador" component={Administrador} />
         <Route exact path="/404" component={PageNotFound} />
         <Route component={PageNotFound} />
       </Switch>
--- client/src/FoodAdd/pages/Administrador.js
+++ client/src/FoodAdd/pages/Administrador.js
@@ -1,67 +1,66 @@
-import React, { useState,useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Administrador = () => {
-    const [users,setUser]=useState([]);
+  const [users, setUser] = useState([]);
 
-    useEffect(() => {
-        loadUsers();
-    }, [])
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-    const loadUsers = async ()=>{
-        const result=await axios.get("http://localhost:3003/users");
-        setUser(result.data.reverse());
-        //console.log(result);
-    } 
-    return ( 
-          <div>
-            <br/>
-              <h2 align="center">MIS PUBLICACIONES</h2>
-               <br/><br/>
-              <div className="ui two column grid ">
-   
-               <div className="column">
-               <table className="ui celled table">
-                    <thead class="thead-dark">
-                       <tr>
-                       <th >#</th>
-                       <th >Nombres</th>
-     
-                      
-                      </tr>
-                    </thead>
-                    <tbody>
-                       {
-                       users.map((user,index)=>(
-                      <tr>
-                         <th scope="row">{index+1}</th>
-                         <td>{user.name}</td>
-                       </tr>
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    setUser(result.data.reverse());
+    //console.log(result);
+  };
+  return (
+    <div>
+      <br />
+      <h2 align="center">MIS PUBLICACIONES</h2>
+      <br />
+      <br />
+      <div className="ui two column grid ">
+        <div className="column">
+          <table className="ui celled table">
+            <thead class="thead-dark">
+              <tr>
+                <th>#</th>
+                <th>Nombres</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{user.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="column" id="botonesprincipales">
+          <br />
+          <br />
+          <br />
+          <br />
+          <Link className="ui button">Modificar</Link>
+          <br />
+          <br />
+          <Link className="ui button">Eliminar</Link>
+          <br />
+          <br />
+          <Link className="ui button" to="/restaurants/Comida">
+            Resgistrar
+          </Link>
+          <br />
+          <br />
 
-                        ))
-                       }
-                    </tbody>
-                 </table>
-               </div>
-               <div className="column">
-                  <br/><br/><br/><br/>
-               <Link className="ui button" id="botonesprincipales" >Modificar</Link>
-                <br />
-                <br />
-                <Link className="ui button" id="botonesprincipales">Eliminar</Link>
-                <br />
-                <br />
-                <Link className="ui button" id="botonesprincipales" to="/restaurants/Comida">Resgistrar</Link>
-                  <br />
-                <br />
-                
-                <Link className="ui button" id="botonesprincipales">Buscar</Link>
-                </div>
-               </div>
-          </div>
-        
-     );
-}
- 
+          <Link className="ui button">Buscar</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Administrador;
