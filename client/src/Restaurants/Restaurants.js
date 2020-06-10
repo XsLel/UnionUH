28997811e5dfdffb46dbfade93ec9d57d7a61fe8
Brian Vega <brian@tivelabs.com>
2020-06-09 20:19:30 -0400

    - se añadio el componente para filtrar restaurantes

--- client/src/Restaurants/Restaurants.js
+++ client/src/Restaurants/Restaurants.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import CardRestaurant from '../components/restaurant/CardRestaurant';
-import { Grid } from 'semantic-ui-react';
-import { TopMenu } from '../Admin/layout';
-import './Restaurants.css';
-import { http } from '../services';
-import Restaurant from '../components/restaurant/Restaurant';
+import React, { Component } from "react";
+import CardRestaurant from "../components/restaurant/CardRestaurant";
+import { Grid } from "semantic-ui-react";
+import { TopMenu } from "../Admin/layout";
+import "./Restaurants.css";
+import { http } from "../services";
+import Restaurant from "../components/restaurant/Restaurant";
+import { restaurantsMocks } from "../services/mocks";
 
 class Restaurants extends Component {
   state = {
     restaurants: [],
+    restaurantsAux: [],
     loading: true,
     viewMain: true,
   };
@@ -16,11 +18,15 @@ class Restaurants extends Component {
   componentDidMount() {
     http
       .request({
-        url: '/restaurantes/restaurants',
+        url: "/restaurantes/restaurants",
       })
       .then((restaurants) => {
         if (restaurants) {
-          this.setState({ restaurants: restaurants, loading: false });
+          this.setState({
+            restaurants: restaurantsMocks,
+            restaurantsAux: restaurantsMocks,
+            loading: false,
+          });
         }
       })
       .catch((error) => {
@@ -31,6 +37,18 @@ class Restaurants extends Component {
       });
   }
 
+  filterRestaurant(text) {
+    const { restaurants, restaurantsAux } = this.state;
+    if (text.length > 0) {
+      const filterRestaurant = restaurantsAux.filter((restaurant) =>
+        restaurant.name.toUpperCase().includes(text.toUpperCase())
+      );
+      this.setState({ restaurantsAux: filterRestaurant });
+    } else {
+      this.setState({ restaurantsAux: restaurants });
+    }
+  }
+
   gridRestaurants(restaurants) {
     return (
       <Grid columns={3} divided={true}>
@@ -38,7 +56,7 @@ class Restaurants extends Component {
           {restaurants.map((restaurant, index) => {
             return (
               <CardRestaurant
-                customClass={'card-Restaurants'}
+                customClass={"card-Restaurants"}
                 key={index}
                 restaurant={restaurant}
               />
@@ -51,11 +69,11 @@ class Restaurants extends Component {
 
   listRestaurants(restaurants) {
     return (
-      <div className={'restaurants'}>
+      <div className={"restaurants"}>
         {restaurants.map((restaurant, index) => {
           return (
             <Restaurant
-              customClass={'card-Restaurants-list'}
+              customClass={"card-Restaurants-list"}
               key={index}
               restaurant={restaurant}
             />
@@ -66,14 +84,14 @@ class Restaurants extends Component {
   }
 
   renderRestaurants() {
-    const { restaurants, viewMain } = this.state;
-    if (restaurants.length === 0) {
+    const { restaurantsAux, viewMain } = this.state;
+    if (restaurantsAux.length === 0) {
       return <h1>No existen restaurantes</h1>;
     }
     if (viewMain) {
-      return this.gridRestaurants(restaurants);
+      return this.gridRestaurants(restaurantsAux);
     } else {
-      return this.listRestaurants(restaurants);
+      return this.listRestaurants(restaurantsAux);
     }
   }
 
@@ -81,24 +99,24 @@ class Restaurants extends Component {
     const { loading, viewMain } = this.state;
     return (
       <div>
-        <TopMenu />
+        <TopMenu
+          showSearchRestaurant={true}
+          filterRestaurant={(text) => this.filterRestaurant(text)}
+        />
         <Grid>
           <Grid.Row>
             <Grid.Column width={4}>
               <h1>Filtros</h1>
             </Grid.Column>
             <Grid.Column width={11}>
-              <div className={'container-Restaurants'}>
+              <div className={"container-Restaurants"}>
                 <div className="title">
                   <h1>Restaurantes</h1>
-                  <div id={'btn-change-list'}>
+                  <div id={"btn-change-list"}>
                     <button
                       className="circular ui basic icon button"
-                      onClick={() => this.changeView()}
-                    >
-                      <i
-                        className={viewMain ? 'icon align justify' : 'icon th'}
-                      />
+                      onClick={() => this.changeView()}>
+                      <i className={viewMain ? "icon align justify" : "icon th"} />
                     </button>
                   </div>
                 </div>
