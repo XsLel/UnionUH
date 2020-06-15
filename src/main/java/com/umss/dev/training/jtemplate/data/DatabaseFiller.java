e25aa6ea0a14ca5d7694060b8211a583ca0682d4
lbct95 <lbct95@gmail.com>
2020-06-15 19:05:51 -0400

    Seed Script

---
+++ src/main/java/com/umss/dev/training/jtemplate/data/DatabaseFiller.java
@@ -0,0 +1,14 @@
+package com.umss.dev.training.jtemplate.data;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFiller {
+    
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        System.out.println("***************** SEEDING DATABASE **********************");
+    }
+}
