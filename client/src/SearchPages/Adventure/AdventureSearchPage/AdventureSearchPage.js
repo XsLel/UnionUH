3899b702975e399e95e07f131b2d3d745cd49264
lbct95 <lbct95@gmail.com>
2020-06-16 19:09:46 -0400

    Fix Frontend Bugs

--- client/src/SearchPages/Adventure/AdventureSearchPage/AdventureSearchPage.js
+++ client/src/SearchPages/Adventure/AdventureSearchPage/AdventureSearchPage.js
@@ -1,8 +1,10 @@
-import { Segment, Item, Container, Divider, Label } from "semantic-ui-react";
+import "../Styles/AdventureSearchPage.css";
+import { Segment, Item, Container, Divider, Message } from "semantic-ui-react";
 import { DropdownList } from "../Components/DropdownList";
 import { SearchBar } from "../Components/SearchBar";
 import React, { Component } from "react";
 import { http } from "./../../../services";
+import { useToasts } from "react-toast-notifications";
 
 class AdventureSearchPage extends Component {
   constructor(props) {
@@ -44,16 +46,26 @@ class AdventureSearchPage extends Component {
       <Container>
         <Segment loading={loading}>
           <Item.Group>
-            <SearchBar
-              onSearchBarTextEnter={this.refreshSearchContent.bind(this)}
-              onInvalidSearch={this.onInvalidSearch.bind(this)}></SearchBar>
+            <div class="search-bar-item">
+              <SearchBar
+                onSearchBarTextEnter={this.refreshSearchContent.bind(this)}
+                onInvalidSearch={this.onInvalidSearch.bind(this)}
+                addToast={this.props.addToast}></SearchBar>
+            </div>
             <Divider> </Divider>
-            {this.state.dataArray.length <= 0 || this.state.invalidSearch ? (
-              <Label size="massive">
-                {this.state.invalidSearch
-                  ? "Datos inválidos, debe ingresar solo caracteres alfanuméricos"
-                  : "No se ha podido encontrar la aventura, ingrese datos nuevamente"}
-              </Label>
+            {this.state.invalidSearch ? (
+              <Message
+                negative
+                size="massive"
+                header="Error en la búsqueda"
+                content="Datos inválidos, debe ingresar solo caracteres alfanuméricos"
+              />
+            ) : this.state.dataArray.length <= 0 ? (
+              <Message
+                warning
+                size="massive"
+                header="Sin resultados"
+                content="No se ha podido encontrar la aventura, ingrese datos nuevamente"></Message>
             ) : (
               <DropdownList data={this.state.dataArray}></DropdownList>
             )}
@@ -64,4 +76,7 @@ class AdventureSearchPage extends Component {
   }
 }
 
-export default AdventureSearchPage;
+export default () => {
+  const { addToast } = useToasts();
+  return <AdventureSearchPage addToast={addToast} />;
+};
