382b01e56427003ef1b1cd0d33e7e9b004a7941f
DanielUnoSoto <63116502+DanielUnoSoto@users.noreply.github.com>
2020-06-15 22:37:16 -0400

    implementacion de responsive en Gallery.js

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/Gallery.css";
+import { Grid, GridColumn, Image } from "semantic-ui-react";
 
 const Gallery = (props) => {
   let { images } = props;
@@ -9,13 +10,15 @@ const Gallery = (props) => {
   function viewImages(images) {
     if (typeof images !== "undefined") {
       return (
-        <div className="images">
+        <Grid>
           {images.map((c, i) => (
-            <div key={i} className="image-container">
-              <img src={c} alt={c} />
-            </div>
+            <GridColumn computer={4} tablet={8} mobile={16}>
+              <div key={i} className="image-container">
+                <Image src={c} alt={c} />
+              </div>
+            </GridColumn>
           ))}
-        </div>
+        </Grid>
       );
     }
   }
